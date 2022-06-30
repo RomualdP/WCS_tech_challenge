bcb2b9a9d8129fc66f338ab38f7c399260d9c867
Romuald Piquet <romualdpiquet@macbook-pro-de-romuald.home>
2022-06-30 14:27:53 +0200

    adding new favicon

--- frontend/src/components/member-list/member.jsx
+++ frontend/src/components/member-list/member.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Argonautes(props) {
   return (
-    <div className="member">
-      <p>{props.name}</p>
-    </div>
+    <ul className="member">
+      <li>{props.name}</li>
+    </ul>
   );
 }
